aa7fe8aa623d4f8c4a30ef0768cb5dc93e2307c0
hmdev2 <hmdev2@proton.me>
2026-01-30 07:20:03 -0300

    fix: base to ./

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/cooperkall-site/",
+  base: "./",
   plugins: [
     react(),
     tailwindcss(),
